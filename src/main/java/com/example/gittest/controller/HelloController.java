fc4665aeefe1257f4a845714adb095f0b4450d87
TMS <TMS0201@tms.cn>
2023-05-26 11:38:29 +0800

    2nd commit

--- src/main/java/com/example/gittest/controller/HelloController.java
+++ src/main/java/com/example/gittest/controller/HelloController.java
@@ -1,4 +1,8 @@
 package com.example.gittest.controller;
 
 public class HelloController {
+    public String hello() {
+        System.out.println("hello, github");
+        return "hello, github";
+    }
 }
